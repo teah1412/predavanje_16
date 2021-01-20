213d42056af51f9dc17cedb783ea5744e846dfef
John Doe <matea.bertovic@gmail.com>
2021-01-20 16:05:14 +0100

    Prva verzija aplikacije

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route ("/")
+def index():
+    return render_template("index.html")
+
+@app.route ("/about-me")
+def index():
+    return render_template("about-me.html")
+
+
+if __name__ == "__main__":
+    app.run()
